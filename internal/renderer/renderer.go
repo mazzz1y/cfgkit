98c0bd73ccc82a135ac65b845c2d38ee95e47a15
Dmitry Rubtsov <me@mazy.wtf>
2025-06-08 20:31:59 +0600

    recursive template vars resolution

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -9,15 +9,20 @@ import (
 )
 
 type Renderer struct {
-	tmpl       *template.Template
+	tmplStr    string
 	resultType string
-	globalVars map[string]any
-	deviceVars map[string]any
+	vars       Variables
+	funcMap    template.FuncMap
 }
 
 type Result struct {
 	Data        bytes.Buffer
 	ContentType string
+}
+
+type Variables struct {
+	Global map[string]any
+	Device map[string]any
 }
 
 func New(cfg *config.Config, name string) (*Renderer, error) {
@@ -31,36 +36,58 @@ func New(cfg *config.Config, name string) (*Renderer, error) {
 		return nil, fmt.Errorf("template %s not found", d.TemplateName)
 	}
 
-	funcMap := template.FuncMap{
-		"toJSON": toJSON,
-	}
-
-	tmpl, err := template.New("").Funcs(funcMap).Parse(t.Template)
-	if err != nil {
-		return nil, fmt.Errorf("parse template: %w", err)
-	}
-
 	return &Renderer{
-		tmpl:       tmpl,
+		tmplStr:    t.Template,
 		resultType: t.Type,
-		globalVars: cfg.Variables,
-		deviceVars: d.Variables,
+		vars: Variables{
+			Global: cfg.Variables,
+			Device: d.Variables,
+		},
+		funcMap: template.FuncMap{
+			"toJSON": toJSON,
+		},
 	}, nil
 }
 
 func (r *Renderer) Render() (*Result, error) {
-	buf := &bytes.Buffer{}
-	data := struct {
-		Global map[string]any
-		Device map[string]any
-	}{
-		Global: r.globalVars,
-		Device: r.deviceVars,
-	}
-	if err := r.tmpl.Execute(buf, data); err != nil {
-		return nil, fmt.Errorf("render: %w", err)
+	buf, err := r.renderLoop()
+	if err != nil {
+		return nil, err
 	}
 
+	return r.format(buf)
+}
+
+func (r *Renderer) renderLoop() (*bytes.Buffer, error) {
+	var prevResult string
+	currentTmplStr := r.tmplStr
+	buf := &bytes.Buffer{}
+
+	for {
+		buf.Reset()
+
+		tmpl, err := template.New("").Funcs(r.funcMap).Parse(currentTmplStr)
+		if err != nil {
+			return nil, fmt.Errorf("parse template: %w", err)
+		}
+
+		if err := tmpl.Execute(buf, r.vars); err != nil {
+			return nil, fmt.Errorf("render: %w", err)
+		}
+
+		newResult := buf.String()
+		if prevResult == newResult {
+			break
+		}
+
+		prevResult = newResult
+		currentTmplStr = newResult
+	}
+
+	return buf, nil
+}
+
+func (r *Renderer) format(buf *bytes.Buffer) (*Result, error) {
 	switch r.resultType {
 	case "json":
 		var out bytes.Buffer
